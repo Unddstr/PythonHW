3f39a106f27e5f14b88e920041bdc3a7bbce5466
Alexander Panteleev <unddstr@gmail.com>
2023-05-26 18:59:08 +0300

    add task4

---
+++ HomeWork1/Task4.py
@@ -0,0 +1,4 @@
+number = int(input("Введите колличество журавликов: "))
+print("Петя сделал: ", int(number / 6))
+print("Катя сделала: ", int(number / 6 * 4))
+print("Сережа сделал: ", int(number / 6))
